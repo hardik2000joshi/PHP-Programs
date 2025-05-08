9704238a00f101bccdb9de68b2aece5a4a42bce0
hardik2000joshi <165023667+hardik2000joshi@users.noreply.github.com>
2025-05-08 02:22:47 -0700

    Add files via upload
    Updated file of array with some changes

--- Array.php
+++ Array.php
@@ -11,21 +11,32 @@
     // Arrays: used to store multiple values in a single variable
     $abc=array("cold drink", "Chocolate cake", "pizza", "pasta", "burger", "waffers", "vanilla ice cream");
     var_dump($abc); // output: array(7) { [0]=> string(10) "cold drink" [1]=> string(13) "Chocolate cake" [2]=> string(5) "pizza" [3]=> string(5) "pasta" [4]=> string(6) "burger" [5]=> string(7) "waffers" [6]=> string(17) "vanilla ice cream" }
-    /*echo $abc[0]; // output: pizza
+    // var_dump() used to display both value and data type of the array
     echo "<br>";
-    echo $abc[1]; // output: pasta
+    echo "Total length of array is: ", count($abc); // output: Total length of array is: 7
+    /*echo "Total length of array is: ", $abc.count();*/ // wrong Method
     echo "<br>";
-    echo $abc[2]; // output: waffers
+    echo "Total size of array is: ", sizeof($abc); // output: Total size of array is: 7
     echo "<br>";
-    echo $abc[3]; // output: vanilla ice cream
+    echo "Array element at index 0 is: ", $abc[0]; // output: Array element at index 0 is: cold drink
     echo "<br>";
-    echo $abc[4]; // output: cold drink
+    echo "Array element at index 1 is: ", $abc[1]; // output: Array element at index 1 is: Chocolate cake
     echo "<br>";
-    echo $abc[5]; // output: Chocolate cake
+    echo "Array element at index 2 is: ", $abc[2]; // output: Array element at index 2 is: pizza
     echo "<br>";
-    echo $abc[6]; // output: burger
+    echo "Array element at index 3 is: ", $abc[3]; // output: Array element at index 3 is: pasta
     echo "<br>";
-    echo $abc.count($abc); // output: 7*/
-    ?>
+    echo "Array element at index 4 is: ", $abc[4]; // output: Array element at index 4 is: burger
+    echo "<br>";
+    echo "Array element at index 5 is: ", $abc[5]; // output: Array element at index 5 is: waffers
+    echo "<br>";
+    echo "Array element at index 6 is: ", $abc[6]; // output: Array element at index 6 is: vanilla ice cream
+    echo "<br>";
+    //loop through the array
+    for ($i=0; $i<=6; $i++){
+        echo "<br>";
+        echo $abc[$i]; // output: Array elements are: cold drink, Chocolate cake, pizza, pasta, burger, waffers, vanilla ice cream
+    }
+   ?>
 </body>
 </html>
