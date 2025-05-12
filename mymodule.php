721f141870b10945e202884898045fb7f0dc8d71
hardik2000joshi <165023667+hardik2000joshi@users.noreply.github.com>
2025-05-12 05:08:56 -0700

    Updated PHP file of mymodule
    Creating plugins for prestaShop

--- mymodule.php
+++ mymodule.php
@@ -13,22 +13,22 @@
         exit;
     }
     class MyModule extends Module{
-        public function_exists(){
+        public function_construct() {
             $this->name='mymodule';
             $this->tab='front_office_features';
             $this->version='1.0.0';     
-            $this->author='';
+            $this->author='Hardik Joshi';
             $this->need_instance=0;
             // $this->need_instance  determines whether module class should be instaniated(to initialize object from class)
-            // when displaying modules page in back office
-            $this->ps_versions_compliancy=[
+            // whether to load the module’s class when displaying the “Modules” page in the back office
+            $this->ps_versions_compliancy=[  // which version of PrestaShop this module is compatible with
                 'min' => '1.7.0.0',
                 'max' => '8.99.99',
             ];
             $this->bootstrap=true;
             // By setting bootstrap= true, informing PrestaShop that your module's back office interface should be rendered
             // using Bootstrap-compatible HTML and CSS
-            parent::_exists();
+            parent::_construct();
 
             $this->displayName=$this->trans('My Module', [], 'Modules.Mymodule.Admin');
             $this->description=$this->trans('Description of my module.', [], 'Modules.Mymodule.Admin'); 
@@ -38,14 +38,31 @@
         }
 
         public function install(){
-            return parent::install();
+            if(Shop::isFeatureActive()){  // to check whether multi-store feature is active or not
+                Shop::setContext(Shop::CONTEXT_ALL); // changes the context in order to apply coming changes to all existing stores
+                 /*Multistore allows merchants to manage multiple shops from single back office
+                 returns true if multistore is active
+                 returns false if not active*/  
+            }
+            return (
+                parent::install() // :: scope resolution operator to access static methods
+                && Configuration::updateValue('MYMODULE_NAME', 'my module') // updating the value in configuration
+            );
         }
+        
         public function uninstall(){
-            return parent::uninstall();
+            // uninstall() method that would delete the data which added to the database during installation
+            return(
+                parent::uninstall()
+                && Configuration::deleteByName('MYMODULE_NAME')
+            )
+            }
+
         }
         
 
-    }
+    
+
          
     ?>
 </body>
